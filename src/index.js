eea653c9a1f6aa22d4d64a3e72366d5b1f52a879
rhazama <rhazama01@gmail.com>
2022-10-19 16:11:04 -0700

    cleaning

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// client side rendered app: react (cra)
+  // -> database which is firebase
+  // -> react-loading-skeleton
+
+// architecture
+  // -> components,
+  // -> constants,
+  // -> context,
+  // -> helpers,
+  // -> lib (firebase is going to live in here),
+  // -> services (firebase functions in here),
+  // styles (tailwind's folder (app/tailwind))
+
